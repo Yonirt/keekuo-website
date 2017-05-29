b6256a1147cdb90ee6ba031cae5ae84be690170e
Mario Lemes Medina <Mario Lemes Medina>
2017-05-29 12:44:41 +0100

    Uploading web template

--- server.js
+++ server.js
@@ -12,15 +12,19 @@ router.get("/",function(req,res){
   res.sendFile(path + "index.html");
 });
 
-router.get("/about",function(req,res){
+/*router.get("/about",function(req,res){
   res.sendFile(path + "about.html");
 });
 
 router.get("/contact",function(req,res){
   res.sendFile(path + "contact.html");
-});
+});*/
 
 app.use("/",router);
+
+//app.use(express.static(__dirname + '/assets'));
+
+app.use("/assets", express.static(__dirname + '/assets'));
 
 /*app.use("*",function(req,res){
   res.sendFile(path + "404.html");
